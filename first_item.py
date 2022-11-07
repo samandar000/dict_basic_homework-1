ba7d28df322d870d1c516989b38845e4076c4718
Samandar000 <samandarnfh.2020@gmail.com>
2022-11-07 22:22:51 +0500

    second attempt

--- first_item.py
+++ first_item.py
@@ -3,5 +3,4 @@ def first_item():
     Given a dictionary, Return first item value.
     """
     data = {'a': 1, 'b': 2}
-    return data['a']
-print(first_item())
+    return  data['a']
--- last_item.py
+++ last_item.py
@@ -3,4 +3,4 @@ def last_item():
     Given a dictionary, Return last item value.
     """
     data = {'a': 1, 'b': 2}
-    return data['b']
+    return data['b'] 
